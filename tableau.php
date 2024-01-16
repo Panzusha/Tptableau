f4976203bf60e015a0369455aada0c786590bdc7
Panzusha <mjdemass@gmail.com>
2024-01-16 21:47:14 +0100

    tp finito

---
+++ tableau.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'users' => [
+        [
+            'name' => 'Maxime Inshape',
+            'email' => 'enorme@sec.fr',
+            'isAdmin' => 'Oui',
+            'id' => 0,
+
+            'contactDetails' => [
+                'address' => [
+                    'rue' => 'rue du dévelopPHP',
+                    'ville' => '36000 FitnessVille',
+                ],
+                'phoneNumber' => '06 42 57 89 53',
+            ],
+
+            'posts' => [
+                [
+                    'id' => 1,
+                    'title' => 'Coucou Steven', 'Damn les gens', 'Boom !', 'On vas tchatcher les petites',
+                    'excerpt' => 'zzrgqgrefzrgqrgqrgrg', 'regrgrgerqgrgrg', 'efefeqzrzgg', 'serfsrgsrgrg',
+
+                    'comments' => [
+                        ['content' => 'zzfrregrgergergergeggreegggreergrg', 'sesrrfezfrgerergegergegegegegg', 'efefszgegegergergergeggergzrrg', 'qefrsgrgsergeergegrgergzrgzg',]
+                    ]
+                ],
+
+            ],
+        ],
+    ],
+];
